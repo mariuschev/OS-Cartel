d2ee38fc1a25b3c250f1c2a6e3b21090681c6745
ConstanceWk <constance-walusiak@outlook.fr>
2023-10-29 15:28:52 -0400

    2 cas : seq + para

--- DJalgo.h
+++ DJalgo.h
@@ -4,9 +4,13 @@
 
 #ifndef OS_CARTEL_DJALGO_H
 #define OS_CARTEL_DJALGO_H
+#include <omp.h>
+#include <stdio.h>
+
+int oracleFunction(int );
+int deutschJozsaAlgorithm_seq();
+int deutschJozsaAlgorithm_para();
+int chooseNumber();
 
 #endif //OS_CARTEL_DJALGO_H
 
-int oracleFunction(int );
-int deutschJozsaAlgorithm();
-int chooseNumber();
--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include "DJalgo.h"
+#include <omp.h>
 
 int main (){
-    int result = deutschJozsaAlgorithm();
+    // commence par 0 : constant
+    // commence par 1 : balanced
+
+    int result = deutschJozsaAlgorithm_seq();
+    printf("Result: %d\n", result);
+    int result2 = deutschJozsaAlgorithm_para();
+    printf("Result: %d\n", result2);
     return 0;
 }
 
