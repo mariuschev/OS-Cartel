b7215f9ef7d8d66efb69dc8733d120e040bb70b2
ConstanceWk <constance-walusiak@outlook.fr>
2023-11-05 17:54:16 -0500

    double targeet

--- Grov_para.c
+++ Grov_para.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <pthread.h>
 
-// Définir une structure pour les données spécifiques au thread
 typedef struct {
     int *values;
     int size;
@@ -12,17 +11,19 @@ typedef struct {
     int thread_id;
     int num_threads;
     pthread_barrier_t *barrier;
-    int result_index;
+    int *result_index; // Utiliser un pointeur pour stocker le résultat
 } ThreadData;
 
-// Fonction pour effectuer une itération parallèle de Grover
 void *grover_parallel_iteration(void *arg) {
     ThreadData *data = (ThreadData *)arg;
 
     // Rechercher la valeur cible dans la partie du tableau assignée à ce thread
     for (int i = data->thread_id; i < data->size; i += data->num_threads) {
         if (data->values[i] == data->target) {
-            data->result_index = i;
+            // Stocker l'index dans le résultat et terminer le thread
+            *(data->result_index) = i;
+            pthread_barrier_wait(data->barrier);
+            pthread_exit(NULL);
         }
     }
 
@@ -32,13 +33,15 @@ void *grover_parallel_iteration(void *arg) {
     pthread_exit(NULL);
 }
 
-// Fonction pour exécuter l'algorithme de Grover en parallèle
 int grover_parallel_algorithm(int size, int target, int num_threads, int *values) {
     pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
     pthread_barrier_t barrier;
     pthread_barrier_init(&barrier, NULL, num_threads);
 
-    // Données du thread
+    int result_index = -1; // Variable pour stocker l'index de la première occurrence de la valeur cible
+    // Pointeur vers la variable résultat
+    int *result_pointer = &result_index;
+
     ThreadData *thread_data = (ThreadData *)malloc(num_threads * sizeof(ThreadData));
     for (int i = 0; i < num_threads; i++) {
         thread_data[i].values = values;
@@ -47,29 +50,17 @@ int grover_parallel_algorithm(int size, int target, int num_threads, int *values
         thread_data[i].thread_id = i;
         thread_data[i].num_threads = num_threads;
         thread_data[i].barrier = &barrier;
-        thread_data[i].result_index = -1; // Initialiser à -1, indiquant que la valeur n'a pas encore été trouvée
+        thread_data[i].result_index = result_pointer; // Passer le pointeur
     }
 
-    // Créer et exécuter les threads
     for (int i = 0; i < num_threads; i++) {
         pthread_create(&threads[i], NULL, grover_parallel_iteration, (void *)&thread_data[i]);
     }
 
-    // Attendre que les threads se terminent
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    // Trouver le premier index non -1 (valeur trouvée) parmi les threads
-    int result_index = -1;
-    for (int i = 0; i < num_threads; i++) {
-        if (thread_data[i].result_index != -1) {
-            result_index = thread_data[i].result_index;
-            break;
-        }
-    }
-
-    // Libérer la mémoire allouée
     free(threads);
     free(thread_data);
     pthread_barrier_destroy(&barrier);
